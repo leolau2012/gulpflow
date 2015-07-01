2ffd8428f200c65756ab02bae40363e422c33a59
--leolau2012 <--leolau2012@gmail.com>
2015-07-01 11:27:41 +0800

    fsafasf

---
+++ src/js/a.js
@@ -0,0 +1,9 @@
+// a.js
+function demo (msg) {
+    alert('--------\r\n' + msg + '\r\n--------')
+}
+function aaa(){
+	// wo jia 我家一些中文主导
+	var a = 1;
+	var bbb = fsdfsdfsafsa;
+}
---
+++ src/js/log.js
@@ -0,0 +1,6 @@
+log('gulp-book' );
+log('fsdfsd');
+function a(){
+
+	alert("我是你大爷");alert("我是你大爷");alert("我是你大爷");
+}
